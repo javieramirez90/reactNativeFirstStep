49c57687553e932dd757c62ed7a15e7b7a7a2abb
javieramirez90 <javier.ramirez@aiesec.net>
2019-07-08 23:49:32 -0500

    Setting up redux

---
+++ src/store/reducers/root.js
@@ -0,0 +1,15 @@
+const initialState = {
+  placeName : '',
+  places : [],
+  selectedPlace: null
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    
+    default:
+      return state;
+  }
+};
+
+export default reducer
