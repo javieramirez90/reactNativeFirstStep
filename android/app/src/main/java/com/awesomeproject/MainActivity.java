0b7e460255ee0968d1f24857c8e6b842143fc1f9
javieramirez90 <javier.ramirez@aiesec.net>
2019-07-12 01:51:09 -0500

    React-native-navigation setup

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -2,14 +2,6 @@ package com.awesomeproject;
 
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "AwesomeProject";
-    }
+public class MainActivity extends com.reactnativenavigation.controllers.SplashActivity {
+    
 }
